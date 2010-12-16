baea5de7f58342f0362f84cb8444260cc47b9273
Mark Striemer <mstriemer@gmail.com>
2010-12-15 18:22:04 -0600

    Iterative working

--- fp.cpp
+++ fp.cpp
@@ -11,8 +11,9 @@
 //#include <mpi.h>
 
 #define LINE_BUF_SIZE 512
-#define MIN_SUP 1
-#define DOMAIN 1000
+// #define MINSUP 2
+// #define DOMAIN 1000
+// #define FILENAME "PreciseDB.txt"
 
 using namespace std;
 
@@ -39,83 +40,79 @@ class TreeElement
 		}
 		return NULL;
 	}
+	
+	TreeElement* print(int nParents)
+	{
+		if (item.itemID != 0)
+		{
+			for (int i = 0; i < nParents; i++)
+				printf("\t");
+				
+			printf("%d:%d\n", item.itemID, item.sup);
+		}
+		
+		for (int i = 0; i < children.size(); i++)
+			children[i]->print(nParents + 1);
+	}
 };
 
-int compare(const void *a, const void *b) 
+int compare(const void *a, const void *b)
 { 
     hElement *ia = (hElement *)a;
     hElement *ib = (hElement *)b;
     return (int)(ib->sup - ia->sup);
 }
 
-
-
 int main(int argc, char *argv[] )
-{	
-	hElement header[DOMAIN];
+{
+	if (argc != 4)
+	{
+		printf("Usage: %s <db_file> <minsup> <max_domain>\n", argv[0]);
+		exit(1);
+	}
+	
+	char *filename = argv[1];
+	int minsup = atoi(argv[2]);
+	int domain = atoi(argv[3]);
+	
+	hElement header[domain];
+	for (int i = 0; i < domain; i++)
+	{
+		header[i].sup = 0;
+		header[i].itemID = i + 1;
+	}
 	int headerCount = 0;
 	
 	//scan DB once, find frequent 1-itemset
-	FILE *db = fopen("PreciseDB.txt","r");
+	FILE *db = fopen(filename,"r");
 	char line[LINE_BUF_SIZE];
 	int  tid;
 	int  itemCount;
-	char *items;  
+	char *itemstr;  
 	char *item;
 	
 	fgets(line, LINE_BUF_SIZE, db);
-	printf("Database transactions: %s",line);
+	// printf("Database transactions: %s",line);
 	
 	while(fgets(line, LINE_BUF_SIZE, db))
 	{
 		//split up the line by tab
 		tid       = atoi(strtok (line,"\t"));
 		itemCount = atoi(strtok (NULL,"\t"));
-		items     = strtok (NULL,"\t");
+		itemstr     = strtok (NULL,"\t");
 		
-		//printf("TID: %d,Item Count: %d,Items: %s",tid,itemCount,items);
+		// printf("TID: %d,Item Count: %d,Items: %s",tid,itemCount,items);
 		
-		
-		item = strtok (items," ");
+		item = strtok (itemstr," ");
 		
 		//parse the items
 		while(item != NULL)
 		{
-			int temp = atoi(item);
-			
-			if(header[temp-1].sup == 0)
-			{
-				headerCount++;
-			}
-			
-			header[temp-1].sup++;
-			header[temp-1].itemID = temp;
-			
-			//printf(">: %s\n",item);
+			// hElement *hEl = &header[atoi(item) - 1];
+			header[atoi(item) - 1].sup++;
 			item = strtok (NULL," ");
 		}
 	}
-	
-	int i;
-	/*
-	for(i = 0; i < DOMAIN; i ++)
-	{
-		printf("%d: %d\n", header[i].item, header[i].sup);
-	}
-	*/
-	
-	//Sort frequent items in frequency descending order into header table
-    //qsort(header, DOMAIN, sizeof(hElement), compare);
-	printf("\n");
-	
-	printf("Header:\n");
-	printf("-------\n");
-	for(i = 0; i < headerCount; i ++)
-	{
-		printf("%d: %d\n", header[i].itemID, header[i].sup);
-	}
-	
-	//discard infrequent items
 	
 	rewind(db);
 	
@@ -134,50 +131,67 @@ int main(int argc, char *argv[] )
 		//split up the line by tab
 		tid       = atoi(strtok (line,"\t"));
 		itemCount = atoi(strtok (NULL,"\t"));
-		items     = strtok (NULL,"\t");
+		itemstr   = strtok (NULL,"\t");
+		hElement* items[itemCount];
 		
 		//printf("TID: %d,Item Count: %d,Items: %s",tid,itemCount,items);
 		
-		item = strtok (items," ");
 		curr = head;
 		
-		
-		
-		//parse the items
-		while(item != NULL)
+		item = strtok (itemstr," ");
+		// Get the hElements for these itemIDs
+		items[0] = &header[atoi(item) - 1];
+		for (int i = 1; i < itemCount; i++)
 		{
-			int tempID = atoi(item);
-			
-			//check for an instance of item
-			TreeElement *tempNode = curr->find(tempID);
-			
-			if(tempNode != NULL)
+			item = strtok (NULL," ");
+			items[i] = &header[atoi(item) - 1];
+		}
+		
+		// Bubble sort the elements into their sup order
+		bool sorted = false;
+		while (!sorted)
+		{
+			sorted = true;
+			for (int i = 1; i < itemCount; i++)
+			{
+				if (items[i]->sup > items[i-1]->sup)
+				{
+					hElement* t = items[i];
+					items[i] = items[i-1];
+					items[i-1] = t;
+					sorted = false;
+				}
+			}
+		}
+		
+		// Go through the elements, while they meet minsup
+		for (int i = 0; i < itemCount && items[i]->sup >= minsup; i++)
+		{
+			// Check if this node exists
+			TreeElement *tempNode = curr->find(items[i]->itemID);
+	
+			if (tempNode != NULL)
 			{
 				tempNode->item.sup++;
 			}
-			else//if it doesn't exist, make it.
+			else // It doesn't exist, make it
 			{
 				tempNode = new TreeElement;
 				tempNode->item.sup = 1;
-				tempNode->item.itemID = tempID;
+				tempNode->item.itemID = items[i]->itemID;
 				curr->children.push_back(tempNode);
 			}
 			
 			curr = tempNode;
-			
-			//printf(">: %s\n",item);
-			item = strtok (NULL," ");
 		}
 	}
 	
-	
-	
 	//printout the tree
-	for(int i = 0; i < head->children.size(); i++)
-	{
+	head->print(0);
+	// for(int i = 0; i < head->children.size(); i++)
+	// {
 		//printf("> %d:%d \n", head->children[i]->item.itemID,head->children[i]->item.sup);
-	}
+	// }
 	
-	
-	return 0;
+	exit(0);
 }
