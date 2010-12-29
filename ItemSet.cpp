b8fdfed0f0fff416a6c5c88c2fb75c36215a67a7
Mark Striemer <mstriemer@gmail.com>
2010-12-29 17:44:41 -0600

    Fixed ItemSet

--- ItemSet.cpp
+++ ItemSet.cpp
@@ -3,41 +3,45 @@
 
 ItemSet::ItemSet(int item)
 {
-	items = new int[1];
-	items[0] = item;
 	size = 1;
+	items = new int[size];
+	items[0] = item;
 }
 
 ItemSet::ItemSet(int item, ItemSet *set)
 {
-	int i;
+	bool itemAdded = false;
+	int j = 0;
+	size = set->size + 1;
 	items = new int[size];
-	for (i = 0; i < size; i++)
-	
-	for (i = 0; i < size-1; i++)
+	for (int i = 0; i < size; i++)
 	{
-		items[i] = set->items[i];
+		if (itemAdded || (j < set->size && set->items[j] < item))
+		{
+			items[i] = set->items[j];
+			j++;
+		}
+		else
+		{
+			items[i] = item;
+			itemAdded = true;
+		}
 	}
-	
-	items[i] = item;
 }
 
 void ItemSet::printSet()
 {
 	for(int i = 0; i < size; i++)
-	{
 		printf("%i ",items[i]);
-	}
 	printf("\n");
 }
 
-bool ItemSet::compare(ItemSet* set)
+bool ItemSet::compare(ItemSet *set)
 {
-	if(size != set->size)
+	if (size != set->size)
 		return false;
 	for (int i = 0; i < size; i++)
+		if (items[i] != set->items[i])
 			return false;
-		if(items[i] != set->items[i])
-			return false;
-	
 	return true;
+}
